1018f34230a66ed5eca7bc3cc3c15459de649840
Rishabh Sinha <02sinharishabh15@gmail.com>
2025-03-08 09:35:30 +0530

    Added persistance and different nanoid

--- controller/dbconnect.js
+++ controller/dbconnect.js
@@ -1,14 +1,38 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import { MongodbPersistence } from "y-mongodb-provider";
 
-export default function dbConnect(url) {
-    mongoose.connect(url, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(() => {
-        console.log('Connected to MongoDB');
-    })
-    .catch((error) => {
-        console.log('Error connecting to MongoDB', error);
+export default async function dbConnect(url) {
+  mongoose.set("strictQuery", true);
+
+  try {
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
+
+    console.log("Connected to MongoDB");
+
+    const db = mongoose.connection.db;
+    const client = mongoose.connection.getClient(); // Get the raw MongoDB client
+
+    // Ensure db and client exist before initializing persistence
+    if (!db || !client) {
+      throw new Error("MongoDB connection not initialized properly.");
+    }
+
+    // Create the Yjs MongoDB Persistence instance
+    const mdb = new MongodbPersistence(
+      { client, db },
+      {
+        collectionName: "yjsDocs",
+        flushSize: 400,
+        multipleCollections: false,
+      }
+    );
+
+    return mdb; // Ensure this is returned properly
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw error; // Rethrow error so that the calling function knows it failed
+  }
 }
--- index.js
+++ index.js
@@ -7,11 +7,10 @@ import dotenv from "dotenv";
 import http from "http";
 import { WebSocketServer } from "ws";
 import { createRequire } from "module";
-import mongoose from "mongoose";
-
 const require = createRequire(import.meta.url);
-const { setupWSConnection } = require("./utils/utils.cjs");
-mongoose.set("strictQuery", true);
+const { setupWSConnection, setPersistence } = require("./utils/utils.cjs");
+import * as Y from "yjs";
+import cors from "cors";
 
 dotenv.config();
 const PORT = 8000;
@@ -19,10 +18,12 @@ const HOST = "0.0.0.0";
 
 const app = express();
 const server = http.createServer(app);
+const mdb = await dbConnect("mongodb://localhost:27017/one-vichaar");
 
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
-dbConnect("mongodb://localhost:27017/one-vichaar");
+
 app.use(logReqRes("log.txt"));
 app.use("/", router);
 
@@ -31,6 +32,55 @@ const wss = new WebSocketServer({ noServer: true });
 wss.on("connection", (ws, req) => {
   console.log("New client connected:", req.url);
   setupWSConnection(ws, req);
+});
+
+
+setPersistence({
+  bindState: async (docName, ydoc) => {
+    const persistedYdoc = await mdb.getYDoc(docName);
+    // get the state vector so we can just store the diffs between client and server
+    const persistedStateVector = Y.encodeStateVector(persistedYdoc);
+
+    /* we could also retrieve that sv with a mdb function
+     *  however this takes longer;
+     *  it would also flush the document (which merges all updates into one)
+     *   thats prob a good thing, which is why we always do this on document close (see writeState)
+     */
+    //const persistedStateVector = await mdb.getStateVector(docName);
+
+    // in the default code the following value gets saved in the db
+    //  this however leads to the case that multiple complete Y.Docs are saved in the db (https://github.com/fadiquader/y-mongodb/issues/7)
+    //const newUpdates = Y.encodeStateAsUpdate(ydoc);
+
+    // better just get the differences and save those:
+    const diff = Y.encodeStateAsUpdate(ydoc, persistedStateVector);
+
+    // store the new data in db (if there is any: empty update is an array of 0s)
+    if (
+      diff.reduce(
+        (previousValue, currentValue) => previousValue + currentValue,
+        0
+      ) > 0
+    )
+      mdb.storeUpdate(docName, diff);
+
+    // send the persisted data to clients
+    Y.applyUpdate(ydoc, Y.encodeStateAsUpdate(persistedYdoc));
+
+    // store updates of the document in db
+    ydoc.on("update", async (update) => {
+      mdb.storeUpdate(docName, update);
+    });
+
+    // cleanup some memory
+    persistedYdoc.destroy();
+  },
+  writeState: async (docName, ydoc) => {
+    // This is called when all connections to the document are closed.
+
+    // flush document on close to have the smallest possible database
+    await mdb.flushDocument(docName);
+  },
 });
 
 server.on("upgrade", (request, socket, head) => {
